a2e51deebf2d92fa72ecb3188d05a9c2acc57c3e
sufubo <731644018@qq.com>
2016-08-31 18:11:16 +0800

    from

--- react/router/modules/Inbox.js
+++ react/router/modules/Inbox.js
@@ -2,6 +2,18 @@ import React from 'react'
 import NavLink from './NavLink'
 
 class Inbox extends React.Component{
+  contextTypes: {
+    router: React.PropTypes.object
+  }
+
+  handleSubmit(event){
+    event.preventDefault();
+    const userName = event.target.elements[0].value;
+    const repo = event.target.elements[1].value;
+    const path = `/inbox/${userName}/${repo}`;
+    this.context.push(path);
+  }
+
   render() {
     return (
       <div>
@@ -9,6 +21,13 @@ class Inbox extends React.Component{
         <ul>
           <li><NavLink to="/repos/hello/react-router">React Router</NavLink></li>
           <li><NavLink to="/repos/world/react">React</NavLink></li>
+          <li>
+             <from onSubmit={this.handleSubmit}>
+                <input placeholder="userName"/>
+                <input placeholder="repo"/>
+                <button type="submit">go</button>
+            </from>
+          </li>
         </ul>
         {this.props.children}
       </div>
--- react/router/modules/Message.js
+++ react/router/modules/Message.js
@@ -2,9 +2,13 @@ import React from 'react'
 
 // 新建一个组件让其在 Inbox 内部渲染
 class Message extends React.Component{
-  render() {
-    return <h3>{this.props.params.userName}</h3>
-  }
+
+    render() {
+       const {userName, repoName} = this.props.params;
+
+       return <h3>{userName} / {repoName}</h3>
+    }
 }
 
 export default Message
+
