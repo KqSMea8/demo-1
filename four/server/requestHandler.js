55da8a8b823f2f319394c92c6581d8aad8c6cda1
sufubo <731644018@qq.com>
2016-09-07 15:35:19 +0800

    get and post

--- four/server/requestHandler.js
+++ four/server/requestHandler.js
@@ -1,13 +1,23 @@
 
+//mock一些数据给发送到前端去
 
-function getData () {
+function getData (req, res) {
+
+    var obj = {'name': 'sufubo'};
 
     //从文件中读取数据返回
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.writeHead(200, {'Content-Type': 'text/plain; charset=utf-8'});
+
+    res.write(JSON.stringify(obj));
+    res.end();
 }
 
-function save () {
-
-    //将数据保存到文件
+function saveData (req, res, postData) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.writeHead(200, {'Content-Type': 'text/plain; charset=utf-8'});    //将数据保存到文件
+    res.end('接受到请求了');
 }
 
-exports.save = save;
+exports.saveData = saveData;
+exports.getData = getData;
