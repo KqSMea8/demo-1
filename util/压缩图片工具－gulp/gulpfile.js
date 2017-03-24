d9a13a1997549e67cabe3969deb4ea5a46c70af5
sufubo <731644018@qq.com>
2017-03-24 23:43:17 +0800

    图片压缩工具制作

---
+++ util/压缩图片工具－gulp/gulpfile.js
@@ -0,0 +1,12 @@
+var gulp = require('gulp');
+var imagemin = require('gulp-imagemin');
+var pngquant = require('imagemin-pngquant'); //png图片压缩插件
+
+gulp.task('default', function () {
+    return gulp.src('images/*')
+        .pipe(imagemin({
+            progressive: true,
+            use: [pngquant()] //使用pngquant来压缩png图片
+        }))
+        .pipe(gulp.dest('dist'));
+});
