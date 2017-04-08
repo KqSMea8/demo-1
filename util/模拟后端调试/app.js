3c3612d6944008fe3467a65ab369eb0702ab7aa9
sufubo <731644018@qq.com>
2017-04-08 13:42:35 +0800

    app.js

---
+++ util/模拟后端调试/app.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var  fs = require("fs");
+var app = express();
+
+
+var data = {
+    name: 'sufubo',
+    age: 22
+};
+
+
+app.get('/test', function (req, res) {
+    // 返回json数据
+    res.json(data);
+
+
+
+    // 这是读取文件进行发送到
+    // fs.readFile('./test.html', 'utf8', function(err, data) {
+    //     res.writeHead(200);
+    //     res.write(data, "binary");
+    //     res.end()
+    //     return;
+    // });
+})
+
+
+// 启动服务器
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
