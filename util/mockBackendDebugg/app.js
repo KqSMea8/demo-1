4cbc431c54bf8fe4070db9e2848a5150ceebc389
sufubo <731644018@qq.com>
2017-07-14 17:21:09 +0800

    粒子实现

--- util/mockBackendDebugg/app.js
+++ util/mockBackendDebugg/app.js
@@ -13,8 +13,6 @@ app.get('/test', function (req, res) {
     // 返回json数据
     res.json(data); // 这里就是json数据不用进行转化
 
-
-
     // 这是读取文件进行发送到
     // fs.readFile('./test.html', 'utf8', function(err, data) {
     //     res.writeHead(200);
