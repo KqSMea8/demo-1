5f1ffcb114bd576105529080ed95a6a621411e0f
sufubo <731644018@qq.com>
2017-08-20 11:59:16 +0800

    压缩图片工具

--- Tool/compressPicture-gulp/gulpfile.js
+++ Tool/compressPicture-gulp/gulpfile.js
@@ -1,12 +1,28 @@
 var gulp = require('gulp');
 var imagemin = require('gulp-imagemin');
-var pngquant = require('imagemin-pngquant'); //png图片压缩插件
+var jpegtran = require('imagemin-jpegtran'); // jpg图片压缩插件
+var pngquant = require('imagemin-pngquant'); // png图片压缩插件
 
-gulp.task('default', function () {
-    return gulp.src('images/*')
+// 压缩png
+gulp.task('png', function () {
+    return gulp.src('images/*.png')
         .pipe(imagemin({
             progressive: true,
-            use: [pngquant()] //使用pngquant来压缩png图片
+            use: [pngquant()] // 使用pngquant来压缩png图片
         }))
         .pipe(gulp.dest('dist'));
+});
+
+// 压缩jpg
+gulp.task('jpg', function () {
+    return gulp.src('images/*.jpg')
+        .pipe(imagemin({
+            progressive: true,
+            use: [pngquant()] // 使用pngquant来压缩png图片
+        }))
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('default', function () {
+    return gulp.start('png', 'jpg');
 });
