8a54a22994047dc89962a0295010784fa044bc27
sufubo <731644018@qq.com>
2017-08-24 18:26:42 +0800

    fix watch

--- Tool/watch/watch.js
+++ Tool/watch/watch.js
@@ -2,9 +2,16 @@ let chokidar = require('chokidar');
 let shelljs = require('shelljs');
 let dir = '';
 let command = '';
+let timer = null;
 
 chokidar.watch(dir).on('change', (event, path) => {
-    console.log('exec start');
-    shelljs.exec(command);
-    console.log('exec end');
+    if (!timer) {
+        setTimeout(function () {
+            console.log('exec start');
+            shelljs.exec(command);
+            console.log('exec end');
+            clearTimeout(timer);
+            timer = null;
+        }, 20);
+    }
 });
