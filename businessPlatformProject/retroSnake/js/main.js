607972d4df032d61f895db55604368743619614a
sufubo <731644018@qq.com>
2018-03-03 18:15:21 +0800

    使蛇动起来

--- businessPlatformProject/retroSnake/js/main.js
+++ businessPlatformProject/retroSnake/js/main.js
@@ -1,19 +1,7 @@
 /**
- * 看了演示的知道了需求，我就做个第一关就可以了。后面的也就第三关比较麻烦要有障碍物
- * 不过障碍物也可以实现就是随机在剩下的空间里面去方也就是把某几个连续的点设置为一个标志
- * 可以固定的简单几个固定的方向，固定的长度进行配置
- * 后面需要再想随机的算法。
- * 这里可以参考别人的。或者自己想，可以先考虑一个大概的算法就可以。
- *
- * 我这里只做一个第一关的普通模式就行了。
- * 先思考一下需求。可以用上下左右键控制蛇的方向，然后还有蛇有一个速度，蛇只能往前左右走
- * 在有一个范围内，有一只蛇，然后有要吃的东西，他也随机在棋盘上剩下的空间里。然后蛇要开始移动
- * 然后使用不同的键去控制这个方向，然后要有蛇去吃标记的点，如果蛇碰到标记的点，蛇怎么知道这个
- * 是标记的点，蛇每到一个点就去检测这个点的状态，那这个就是涉及到每个状态的同步。用数组去保存
- * 这些点，然后就把这个点给更新了，给蛇的尾巴长度加1那就是最后一个点。先让蛇运动起来先不要吃东西
- * ，然后一条二个点的蛇。让他运动起来。然后操作方向，怎么动起来，就是点的变化。存起来一些点。然后
- * 高亮，动起来是有规律的。x和y轴
- *
+ * 现在重新开始写贪吃蛇，还有很多地方差一点，现在写到了让蛇显示出来，但是还没有动起来
+ * 现在的目标是先让蛇动起来，开始有一个方向，然后按照这个方向就会有不同的坐标变化
+ * 然后实时的刷新显示就可以了，原来的思路是在改变颜色的过程中就把位置给换了，那也可以，
  *
  * 1.范围
  * 2.初始生成一个蛇，随机在棋盘上，纪录蛇的位置，并把相应位置标记
@@ -27,7 +15,7 @@ var height = 20;
 var gridePanel = document.getElementById('gride-panel');
 var grideWidth = 20;
 var direction = 'up'; // 初始方向  'down':下 left:左 right:右
-var snakeIndexArr = [];
+var snakeIndexArr = []; // [{x: x, y: y}]
 
 function createGride() {
     var rn = 0;
@@ -63,24 +51,55 @@ function createSnake() {
     var y = Math.floor(Math.random() * height);
 
     snakeIndexArr.push({
-        x: x < 10 ? '0' + x : x,
-        y: y < 10 ? '0' + y : y
+        x,
+        y
     });
 
     y = y + 1;
-    y = y > 19 ? '18' : y;
+    y = y > 19 ? 18 : y;
 
     snakeIndexArr.push({
-        x: x < 10 ? '0' + x : x,
-        y: y < 10 ? '0' + y : y
+        x,
+        y
     });
+}
 
-    // 他是把蛇当作一个对象，所以是this，我呢就直接是全局变量了。
+/**
+ * 蛇运动
+ *
+ * 要想使蛇运动起来就是改变坐标根据当前的方向然后改变坐标,怎么改变呢，把最后一个删掉，然后添加
+ * 到最前面的添加
+ *
+ * @return {[type]} [description]
+ */
+function moveSnake() {
+    snakeIndexArr.shift();
+    var lastPos = snakeIndexArr[snakeIndexArr.length - 1];
+    var newPos = {};
+    switch (direction) {
+        case 'up':
+            newPos.x = lastPos.x;
+            newPos.y = lastPos.y - 1;
+            break;
+        case 'down':
+            newPos.x = lastPos.x;
+            newPos.y = lastPos.y + 1;
+            break;
+        case 'left':
+            newPos.x = lastPos.x - 1;
+            newPos.y = lastPos.y;
+            break;
+        case 'right':
+            newPos.x = lastPos.x + 1;
+            newPos.y = lastPos.y;
+            break;
+        default:
+            throw new Error('direction is error');
+    }
 
+    snakeIndexArr.push(newPos);
 
-    // 如果用一个数组，进行记录，在移动的时候改变当前位置，如何和继承联系起来
-    // 就是在新建对象的时候有初始值进行复制
-    // 记录下来位置的点的颜色进行标亮，改变颜色
+    changeColor();
 }
 
 // 输入蛇的坐标，改变相应坐标点的颜色
@@ -89,36 +108,37 @@ function changeColor() {
     var currentId = null;
     var currentIndex = null;
     var headIndex = snakeIndexArr.shift();
-    currentId = 'g-' + headIndex.x + headIndex.y;
-    // console.log(currentId);
+    var currentId = getCurrentClass(headIndex);
     currentDom = document.getElementById(currentId);
     currentDom.className = 'gride';
-
 
     currentIndex = snakeIndexArr[snakeIndexArr.length - 1];
     switch (direction) {
         case 'up':
             snakeIndexArr.push({
                 x: currentIndex.x,
-                y: currentIndex.y < 9
-                    ? '0' + (parseInt(currentIndex.y, 10) + 1) : parseInt(currentIndex.y, 10) + 1
+                y: currentIndex.y + 1
+                    // ? '0' + (parseInt(currentIndex.y, 10) + 1) : parseInt(currentIndex.y, 10) + 1
             });
             break;
         case 'down':
             snakeIndexArr.push({
                 x: currentIndex.x,
-                y: currentIndex.y < 11 ? '0' + (parseInt(currentIndex.y, 10) - 1) : parseInt(currentIndex.y, 10) - 1
+                y: currentIndex.y -1
+                // < 11 ? '0' + (parseInt(currentIndex.y, 10) - 1) : parseInt(currentIndex.y, 10) - 1
             });
             break;
         case 'left':
             snakeIndexArr.push({
-                x: currentIndex.x < 11 ? '0' + (parseInt(currentIndex.x, 10) - 1) : parseInt(currentIndex.x, 10) - 1,
+                x: currentIndex.x - 1,
+                // < 11 ? '0' + (parseInt(currentIndex.x, 10) - 1) : parseInt(currentIndex.x, 10) - 1,
                 y: currentIndex.y
             });
             break;
         case 'right':
             snakeIndexArr.push({
-                x: currentIndex.x < 9 ? '0' + (parseInt(currentIndex.x, 10) + 1) : currentIndex.x + 1,
+                x: currentIndex.x + 1,
+                 // < 9 ? '0' + (parseInt(currentIndex.x, 10) + 1) : currentIndex.x + 1,
                 y: currentIndex.y
             });
             break;
@@ -129,21 +149,40 @@ function changeColor() {
     for (var i = 0; i < snakeIndexArr.length; i++) {
         currentIndex = snakeIndexArr[i];
         console.log(currentIndex);
-        currentId = 'g-' + currentIndex.x + currentIndex.y;
+        var currentId = getCurrentClass(currentIndex);
         console.log(currentId);
         currentDom = document.getElementById(currentId);
         currentDom.className = 'snake';
     }
+}
 
+/**
+ * 获取制定下标的dom元素id
+ *
+ * @param  {array} currentIndex 当前下标对象
+ * @return {string} dom元素id
+ */
+function getCurrentClass(currentIndex) {
+    var x = currentIndex.x;
+    var y = currentIndex.y;
+    var currentX = x < 10 ? '0' + x : x;
+    var currentY = y < 10 ? '0' + y : y;
+    var currentId = 'g-' + currentX + currentY;
+    return currentId;
 }
 
 function init() {
     // 生成一个棋盘
     createGride();
-
     // 生成一个蛇
     createSnake();
+
     changeColor();
+
+    setTimeout(function () {
+        changeColor();
+    }, 2000);
+
 }
 
 init();
