fb8b4df341c60fd8994dac3bb0e69b2d7fc9ef51
sufubo <731644018@qq.com>
2018-03-03 22:18:47 +0800

    使蛇动起来

--- businessPlatformProject/retroSnake/js/main.js
+++ businessPlatformProject/retroSnake/js/main.js
@@ -1,7 +1,6 @@
 /**
- * 现在重新开始写贪吃蛇，还有很多地方差一点，现在写到了让蛇显示出来，但是还没有动起来
- * 现在的目标是先让蛇动起来，开始有一个方向，然后按照这个方向就会有不同的坐标变化
- * 然后实时的刷新显示就可以了，原来的思路是在改变颜色的过程中就把位置给换了，那也可以，
+ * 现在蛇可以动起来了，按照方向进行动。现在已经可以让蛇动起来了。下一步就是要让蛇可以转向
+ * 转向怎么做呢，方向键按下，然后改变对应的direction
  *
  * 1.范围
  * 2.初始生成一个蛇，随机在棋盘上，纪录蛇的位置，并把相应位置标记
@@ -14,8 +13,9 @@ var width = 20;
 var height = 20;
 var gridePanel = document.getElementById('gride-panel');
 var grideWidth = 20;
-var direction = 'up'; // 初始方向  'down':下 left:左 right:右
+var direction = 'right'; // 初始方向  'down':下 left:左 right:右
 var snakeIndexArr = []; // [{x: x, y: y}]
+var speed = 1000; // 单位毫秒
 
 function createGride() {
     var rn = 0;
@@ -26,9 +26,9 @@ function createGride() {
     for (var i = 0; i < width; i++ ) {
         for (var j = 0; j < height; j++) {
             var div = document.createElement('div');
-            rn = i < 10 ? '0' + i : i;
-            cn = j < 10 ? '0' + j : j;
-            div.id = 'g-' + rn + cn; // i行j列
+            rn = i < 10 ? '0' + i : i; // y轴
+            cn = j < 10 ? '0' + j : j; // x轴
+            div.id = 'g-' + cn + rn;
             div.className = 'gride';
             div.style.width = grideWidth + 'px';
             div.style.height = grideWidth + 'px';
@@ -98,62 +98,89 @@ function moveSnake() {
     }
 
     snakeIndexArr.push(newPos);
-
-    changeColor();
 }
 
 // 输入蛇的坐标，改变相应坐标点的颜色
 function changeColor() {
     var currentDom = null;
     var currentId = null;
-    var currentIndex = null;
+    var lastPos = null;
+    var newPos = {};
     var headIndex = snakeIndexArr.shift();
     var currentId = getCurrentClass(headIndex);
     currentDom = document.getElementById(currentId);
     currentDom.className = 'gride';
+    lastPos = snakeIndexArr[snakeIndexArr.length - 1];
 
-    currentIndex = snakeIndexArr[snakeIndexArr.length - 1];
     switch (direction) {
         case 'up':
-            snakeIndexArr.push({
-                x: currentIndex.x,
-                y: currentIndex.y + 1
-                    // ? '0' + (parseInt(currentIndex.y, 10) + 1) : parseInt(currentIndex.y, 10) + 1
-            });
+            newPos.x = lastPos.x;
+            newPos.y = lastPos.y - 1;
             break;
         case 'down':
-            snakeIndexArr.push({
-                x: currentIndex.x,
-                y: currentIndex.y -1
-                // < 11 ? '0' + (parseInt(currentIndex.y, 10) - 1) : parseInt(currentIndex.y, 10) - 1
-            });
+            newPos.x = lastPos.x;
+            newPos.y = lastPos.y + 1;
             break;
         case 'left':
-            snakeIndexArr.push({
-                x: currentIndex.x - 1,
-                // < 11 ? '0' + (parseInt(currentIndex.x, 10) - 1) : parseInt(currentIndex.x, 10) - 1,
-                y: currentIndex.y
-            });
+            newPos.x = lastPos.x - 1;
+            newPos.y = lastPos.y;
             break;
         case 'right':
-            snakeIndexArr.push({
-                x: currentIndex.x + 1,
-                 // < 9 ? '0' + (parseInt(currentIndex.x, 10) + 1) : currentIndex.x + 1,
-                y: currentIndex.y
-            });
+            newPos.x = lastPos.x + 1;
+            newPos.y = lastPos.y;
             break;
         default:
-            return;
+            throw new Error('direction is error');
     }
+
+    snakeIndexArr.push(newPos);
+
+    // switch (direction) {
+    //     case 'up':
+    //         snakeIndexArr.push({
+    //             x: currentIndex.x,
+    //             y: currentIndex.y - 1
+    //         });
+    //         break;
+    //     case 'down':
+    //         snakeIndexArr.push({
+    //             x: currentIndex.x,
+    //             y: currentIndex.y + 1
+    //         });
+    //         break;
+    //     case 'left':
+    //         snakeIndexArr.push({
+    //             x: currentIndex.x - 1,
+    //             y: currentIndex.y
+    //         });
+    //         break;
+    //     case 'right':
+    //         snakeIndexArr.push({
+    //             x: currentIndex.x + 1,
+    //             y: currentIndex.y
+    //         });
+    //         break;
+    // }
 
     for (var i = 0; i < snakeIndexArr.length; i++) {
         currentIndex = snakeIndexArr[i];
         console.log(currentIndex);
         var currentId = getCurrentClass(currentIndex);
+        if (currentIndex.x >= width
+            || currentIndex.y >= width
+            || currentIndex.x < 0
+            || currentIndex.y < 0) {
+            alert('game over');
+            return false;
+        }
         console.log(currentId);
         currentDom = document.getElementById(currentId);
         currentDom.className = 'snake';
     }
+
+    setTimeout(function () {
+        changeColor();
+    }, speed);
 }
 
 /**
@@ -171,18 +198,46 @@ function getCurrentClass(currentIndex) {
     return currentId;
 }
 
+/**
+ * 改变蛇的运动方向
+ *
+ * @return null
+ */
+function changeDirection(e) {
+    var keyCode = e.keyCode;
+    var keyCodeSheet = {
+        left: 37,
+        up: 38,
+        right: 39,
+        down: 40
+    };
+    console.log(keyCode);
+    switch(keyCode) {
+        case keyCodeSheet.left:
+            direction = 'left';
+            break;
+        case keyCodeSheet.up:
+            direction = 'up';
+            break;
+        case keyCodeSheet.right:
+            direction = 'right';
+            break;
+        case keyCodeSheet.down:
+            direction = 'down';
+            break;
+    }
+    console.log(direction);
+}
+
 function init() {
     // 生成一个棋盘
     createGride();
     // 生成一个蛇
     createSnake();
-
+    // 蛇运动
     changeColor();
-
-    setTimeout(function () {
-        changeColor();
-    }, 2000);
-
+    // 改变蛇的运动方向
+    document.addEventListener('keydown', changeDirection);
 }
 
 init();
