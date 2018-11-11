d7a6fa41a157ada8460fc4a665be3afcf34374b0
sufubo <731644018@qq.com>
2018-11-11 10:36:46 +0800

    fs

--- node/test/test.js
+++ node/test/test.js
@@ -1,3 +1,38 @@
+const fs = require('fs');
+const path = require('path');
 
-let test = path.resolve('../remind', 'config.js');
-console.log(test);
+let testPath = path.resolve('../test');
+let res = [];
+
+readFileName(testPath).then(() => {
+    console.log(res);
+});
+
+async function readFileName(filePath) {
+    await fs.readdir(filePath, (err, files) => {
+        if (err) {
+            throw err;
+        }
+        files.forEach((filename, index) => {
+            let fileDir = path.join(filePath, filename);
+            fs.stat(fileDir, (err, stats) => {
+                if (err) {
+                    throw err;
+                }
+                let isFile = stats.isFile();
+                let isDir = stats.isDirectory();
+
+                if (isFile) {
+                    res.push({
+                        [fileDir]: filename
+                    });
+                }
+
+                if (isDir) {
+                    readFileName(fileDir);
+                }
+            })
+        })
+    })
+}
+
