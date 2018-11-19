78200e100732907a6e49025935b1df61e2275460
sufubo <731644018@qq.com>
2018-11-19 23:19:24 +0800

    user

---
+++ asset-server-express/dao/user.js
@@ -0,0 +1,7 @@
+// https://juejin.im/post/5acbeeb7f265da23a049df8a
+// https://blog.csdn.net/Coder_py/article/details/74895003
+
+let user = {
+    insert: 'insert into user(id, name, password) value(0, ?, ?);',
+    update: ''
+}
