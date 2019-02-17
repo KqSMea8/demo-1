e9df4a3392b00c3f8b5cb4d1307ea6c2ca9db03f
sufubo <731644018@qq.com>
2019-02-17 11:21:30 +0800

    新建python目录学习

---
+++ python/main.py
@@ -0,0 +1,16 @@
+#coding=utf-8
+
+import sys, getopt
+
+# def main(argv):
+print('参数个数为:', len(sys.argv), '个参数。')
+print('参数列表：', str(sys.argv))
+print('参数', sys.argv)
+    # for opt, arg in sys.argv:
+    #     if ()
+
+# 1. 分离出来数字和运算符号
+# 2. 判断运算符合进行运算
+
+# if if __name__ == "__main__":
+#     main(sys.argv[1:])
