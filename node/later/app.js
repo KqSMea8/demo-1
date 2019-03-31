5b3c9ada38fd93a9339a305886467d23762dd2af
sufubo <731644018@qq.com>
2019-03-31 23:40:25 +0800

    node 解析json和url

--- node/later/app.js
+++ node/later/app.js
@@ -1,15 +1,21 @@
 const express = require('express');
 const app = express();
 const articles = [{title: 'example'}];
+const bodyParser = require('body-parser');
 
 app.set('port', process.env.PORT || 3000);
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/articles', (req, res, next) => {
     res.send(articles);
 });
 
-app.post('/articels', (req, res, next) => {
-    res.send('pk');
+app.post('/articles', (req, res, next) => {
+    const article = {title: req.body.title};
+    articles.push(article);
+    res.send(article);
 });
 
 app.get('/articles/:id', (req, res, next) => {
